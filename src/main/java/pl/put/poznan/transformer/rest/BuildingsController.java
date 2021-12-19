eb550900c09a18ec8732c9445354345466a41076
D37ERTER <43070341+D37ERTER@users.noreply.github.com>
2021-12-19 20:07:19 +0100

    #16 v2 (delete)

--- src/main/java/pl/put/poznan/transformer/rest/BuildingsController.java
+++ src/main/java/pl/put/poznan/transformer/rest/BuildingsController.java
@@ -11,42 +11,108 @@ public class BuildingsController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingsController.class);
 
+    public Response getArray(int[] address)
+    {
+        if(address.length == 0)
+            return new Response(TextTransformerApplication.compound);
+        try {
+            LocationComposite currComposite = TextTransformerApplication.compound;
+            int i=0;
+            while (i < address.length-1) {
+                currComposite = (LocationComposite) currComposite.at(address[i++]);
+            }
+            return new Response(currComposite.at(address[i]));
+        }catch (Exception e) {
+            if(e.getClass().equals(ClassCastException.class))
+                return new Response("This location can't contain other locations");
+            return new Response(e.getMessage()); // different thrown errors (for example: "composite don't have this index")
+        }
+    }
+
+    public Response deleteArray(int[] address)
+    {
+        if(address.length == 0)
+            return new Response("You can't remove main container");
+        try {
+            LocationComposite currComposite = TextTransformerApplication.compound;
+            int i=0;
+            while (i < address.length-1) {
+                currComposite = (LocationComposite) currComposite.at(address[i++]);
+            }
+            currComposite.remove(address[i]);
+            return new Response(currComposite);
+        }catch (Exception e) {
+            if(e.getClass().equals(ClassCastException.class))
+                return new Response("This location can't contain other locations");
+            return new Response(e.getMessage()); // different thrown errors (for example: "composite don't have this index")
+        }
+    }
+
+
+
+
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     public Response get() {
-        return new Response(TextTransformerApplication.compound);
+        int[] address = {};
+        return getArray(address);
     }
 
     @RequestMapping(value = "/{locationID}", method = RequestMethod.GET, produces = "application/json")
     public Response get(@PathVariable Integer locationID) {
-        try {
-            Location loc = TextTransformerApplication.compound.at(locationID);
-            return new Response(loc);
-        }catch (Exception e) {
-            return new Response(e.getMessage());
-        }
+        int[] address = {locationID};
+        return getArray(address);
     }
 
     @RequestMapping(value = "/{locationID}/{childID}", method = RequestMethod.GET, produces = "application/json")
     public Response get(@PathVariable Integer locationID, @PathVariable Integer childID) {
-        try{
-            LocationComposite com = (LocationComposite) TextTransformerApplication.compound.at(locationID);
-            Location loc = com.at(childID);
-            return new Response(loc);
-        }catch (Exception e) {
-            return new Response(e.getMessage());
-        }
+        int[] address = {locationID, childID};
+        return getArray(address);
     }
 
     @RequestMapping(value = "/{locationID}/{childID1}/{childID2}", method = RequestMethod.GET, produces = "application/json")
     public Response get(@PathVariable Integer locationID, @PathVariable Integer childID1, @PathVariable Integer childID2) {
-        try{
-            LocationComposite com1 = (LocationComposite) TextTransformerApplication.compound.at(locationID);
-            LocationComposite com2 = (LocationComposite) com1.at(childID1);
-            Location loc = com2.at(childID2);
-            return new Response(loc);
-        }catch (Exception e) {
-            return new Response(e.getMessage());
-        }
+        int[] address = {locationID, childID1, childID2};
+        return getArray(address);
+    }
+
+    @RequestMapping(value = "/{locationID}/{childID1}/{childID2}/{childID3}", method = RequestMethod.GET, produces = "application/json")
+    public Response get(@PathVariable Integer locationID, @PathVariable Integer childID1, @PathVariable Integer childID2, @PathVariable Integer childID3) {
+        int[] address = {locationID, childID1, childID2, childID3};
+        return getArray(address);
+    }
+
+
+
+
+
+    @RequestMapping(value = "/", method = RequestMethod.DELETE, produces = "application/json")
+    public Response delete() {
+        int[] address = {};
+        return deleteArray(address);
+    }
+
+    @RequestMapping(value = "/{locationID}", method = RequestMethod.DELETE, produces = "application/json")
+    public Response delete(@PathVariable Integer locationID) {
+        int[] address = {locationID};
+        return deleteArray(address);
+    }
+
+    @RequestMapping(value = "/{locationID}/{childID}", method = RequestMethod.DELETE, produces = "application/json")
+    public Response delete(@PathVariable Integer locationID, @PathVariable Integer childID) {
+        int[] address = {locationID, childID};
+        return deleteArray(address);
+    }
+
+    @RequestMapping(value = "/{locationID}/{childID1}/{childID2}", method = RequestMethod.DELETE, produces = "application/json")
+    public Response delete(@PathVariable Integer locationID, @PathVariable Integer childID1, @PathVariable Integer childID2) {
+        int[] address = {locationID, childID1, childID2};
+        return deleteArray(address);
+    }
+
+    @RequestMapping(value = "/{locationID}/{childID1}/{childID2}/{childID3}", method = RequestMethod.DELETE, produces = "application/json")
+    public Response delete(@PathVariable Integer locationID, @PathVariable Integer childID1, @PathVariable Integer childID2, @PathVariable Integer childID3) {
+        int[] address = {locationID, childID1, childID2, childID3};
+        return deleteArray(address);
     }
 
 //    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
